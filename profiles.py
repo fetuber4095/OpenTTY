643416c3062ec4c9fbf3d71235417b55893a5868
Felipe <felipebr4095@gmail.com>
2023-07-30 16:51:23 +0000

    Added profile template

---
+++ profiles.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+#  
+#  OpenTTY Profile Template for (PSH Virtual Envirronment)
+#  
+
+from opentty import *
+
+
+# Setup your profile
+#
+library['profile'] = "NAME-OF-PROFILE" # Set name of profile
+
+library['debugmode'] = False # Enable or disable OpenTTY [debug mode]
+
+library['alias']['ALIAS-NAME'] = "COMMAND" # Modify "ALIAS-NAME" with asset name and "COMMAND" with alias content 
+library['whitelist'].append("filename") # Add files permanently at whitelist
+
+library['passwd'] = "1234" # Setup your password
+
+library['head-lines'] = 10 # Setup number of file lines that will be show in 'head' and 'tail' commands
+library['max-byte-len'] = 128 # Setup max range for dd txt2bin client and ThreadIn looping
+library['hidden-files-prefix'] = "." # Setup prefix of hidden Files
+
+library['dircolors']['extension'] = "\033[COLORCODEm" # Add and modify files colors
+
+library['resources']['RESOURCE-NAME'] = { # Setup your assets
+    "filename": "NAME-OF-ASSET",
+    "url": "URL-FOR-ASSET-DOWNLOAD"
+}
+
+
+if __name__ == "__main__":
+    app = OpenTTY() 
+
+    app.connect("NAME-OF-SESSION", port=8080) # Start PSH session [PORT is PID of psh at OpenTTY Envirronment]
+
