277c579dab94bf1b84369e59f71650489e86f39c
Felipe <felipebr4095@gmail.com>
2023-07-30 17:32:38 +0000

    Moving profiles to your folders

--- profiles/profiles.py
+++ profiles/profiles.py
@@ -11,6 +11,7 @@ from opentty import *
 library['profile'] = "NAME-OF-PROFILE" # Set name of profile
 
 library['debugmode'] = False # Enable or disable OpenTTY [debug mode]
+library['ipinfo-token'] = "" # Setup your "https://ipinfo.io" acess token for command FW
 
 library['alias']['ALIAS-NAME'] = "COMMAND" # Modify "ALIAS-NAME" with asset name and "COMMAND" with alias content 
 library['whitelist'].append("filename") # Add files permanently at whitelist
