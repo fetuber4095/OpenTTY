69fc7902fe4fb26e1f833ca3c58f4d9d828a0d89
Felipe <felipebr4095@gmail.com>
2023-07-31 01:43:24 +0000

    Start building Pypi dependence

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='opentty',
+    version='1.0',
+    author='Mr. Lima',
+    author_email='felipebr4095@gmail.com',
+    description='The OpenTTY is a Terminal Emulator created tottaly in python based-on Unix-Like Shells',
+    long_description='For more informations see README at Github',
+    long_description_content_type='text/markdown',
+    url='https://github.com/fetuber4095/OpenTTY',
+    packages=['.'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.11',
+    ],
+    keywords='python package, emulator, windows, linux, shell, project',
+    install_requires=[
+        
+    ],
+)
